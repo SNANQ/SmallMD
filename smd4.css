7773e407415cc4e1f430437b244be761951a9f60
ShangHuo <1478664643@qq.com>
2019-01-21 20:43:08 +0800

    Update smd4.css

--- smd4.css
+++ smd4.css
@@ -1,5 +1,5 @@
 /*
-* <one line to give the program's name and a brief idea of what it does.>
+* SmallMD4 . A parser for SMD language. Compatible markdown grammar.
 * Copyright (C) 2019  shanghuo
 * 
 * This program is free software: you can redistribute it and/or modify
